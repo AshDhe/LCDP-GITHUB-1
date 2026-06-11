5171f2942c32323b276587f46677333ce47474f7
AshDhe <157318874+AshDhe@users.noreply.github.com>
2026-06-11 08:01:45 +0200

    Add files via upload

---
+++ OBJETS/CSS/nouvelle-date-membre.css
@@ -0,0 +1,64 @@
+.section-fermeture-formulaire {
+  padding: var(--space-3) 0 0;
+  text-align: right;
+}
+
+#table-nouvelle-date-membre {
+  table-layout: fixed;
+}
+
+#table-nouvelle-date-membre th,
+#table-nouvelle-date-membre td {
+  text-align: center;
+}
+
+#table-nouvelle-date-membre thead th {
+  text-decoration: none;
+}
+
+#titre-departement-membre {
+  display: inline-block;
+  margin-right: var(--space-2);
+  font-size: clamp(24px, 2.4vw, 34px);
+  line-height: 1.1;
+  font-weight: 700;
+  color: var(--color-text);
+}
+
+#bouton-demander-ia {
+  margin: 0 auto;
+}
+
+.carte-parc-membre {
+  text-align: left;
+}
+
+.carte-parc-membre h3 {
+  margin-bottom: var(--space-2);
+}
+
+.carte-parc-membre p {
+  max-width: none;
+  margin-bottom: var(--space-2);
+}
+
+.carte-parc-membre .micro-action {
+  margin-top: var(--space-2);
+}
+
+@media (max-width: 600px) {
+  .section-fermeture-formulaire {
+    text-align: center;
+  }
+
+  #titre-departement-membre {
+    display: block;
+    margin-right: 0;
+    margin-bottom: var(--space-2);
+    font-size: 28px;
+  }
+
+  #bouton-demander-ia {
+    width: 100%;
+  }
+}
